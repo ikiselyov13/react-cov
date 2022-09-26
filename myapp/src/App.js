2942f282968939b61b32ffd015966a1ee7851ea7
ikiselyov13 <ikiselyov@mail.ru>
2022-09-26 17:47:28 +0300

    Сделал п.1 и п.3 из дз 3

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,26 @@
 import logo from './logo.svg';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
-/*function App() {
+
+function App() {
   const userName = 'world'
+
+  const [messageList, setMessageList] = useState([
+    { text: '', author: '' },
+    { text: '', author: '' },
+    { text: '', author: '' }
+  ])
+  const [messageBody, setMessageBody] = useState({
+    text: '',
+    author: ''
+  })
+
+  const RobotMessage = 'Сообщение принято'
+
+  
 
   return (
     <div className="App">
@@ -11,23 +28,22 @@ import './App.css';
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <Message name={userName} id = {1}/>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <Button variant="contained" color="primary">Submit</Button>
+        </p>
       </header>
-      
+      <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
+      <div className="messageList">
+        {
+          messageList.map((e, i) => <Message text={e.text} author={e.author} key={i} />)
+        }
+      </div>
     </div>
   );
 }
 
-export default App;
+
 
 const Message = ({name, id}) => {
   return(
@@ -37,11 +53,47 @@ const Message = ({name, id}) => {
       </h1>
     </div>
     )
-}*/
+}
 
-function App() {
+const Form = ({ data, setData, setMessage }) => {
 
-  const [messageList, setMessageList] = useState([
+  const { text, author } = data
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    if (text.length > 0) {
+      setMessage(pervstate => [...pervstate, { text, author }])
+    }
+    setData(
+      {
+        text: '',
+        author: ''
+      }
+    )
+  }
+
+  return (
+    <form onSubmit={submitForm}>
+      <input placeholder='Имя' value={text} onChange={(e) =>
+        setData(pervstate => ({ ...pervstate, text: e.target.value })
+        )
+      } />
+
+      <input placeholder='Текст' value={author} onChange={(e) =>
+        setData(pervstate => ({ ...pervstate, text: e.target.value })
+        )
+      } />
+      <Button variant="contained" color="primary">Отправить</Button>
+    </form>
+
+  )
+}
+
+export default App;
+
+/*function App() {
+
+   const [messageList, setMessageList] = useState([
     { text: '', author: '' },
     { text: '', author: '' },
     { text: '', author: '' }
@@ -71,6 +123,7 @@ function App() {
       </div>
     </div>
   );
+ 
 }
 
 
@@ -124,4 +177,4 @@ const Message = ({ RobotMessage, id }) => {
       </h1>
     </div>
   )
-}
+}*/
